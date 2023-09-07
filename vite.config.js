b63a0e078081d1f92c494c86d6951dca2698140f
Jenskiii <124568361+Jenskiii@users.noreply.github.com>
2023-09-07 20:22:10 +0200

    changed

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import htmlPurge from "vite-plugin-html-purgecss";
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
-export default {
+export default defineConfig({
   plugins: [htmlPurge()],
-};
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        pricing: resolve(__dirname, "html/pricing.html"),
+        about: resolve(__dirname, "html/about-us.html"),
+      },
+    },
+  },
+});
